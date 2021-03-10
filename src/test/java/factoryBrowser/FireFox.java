0f465e6b2922b1210b785ba42a168b91f31c3e74
Maveto <maveto13@gmail.com>
2021-03-10 00:10:55 -0400

    Selenium usando Firefox

--- src/test/java/factoryBrowser/FireFox.java
+++ src/test/java/factoryBrowser/FireFox.java
@@ -1,10 +1,20 @@
 package factoryBrowser;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class FireFox implements IBrowser{
     @Override
     public WebDriver create() {
-        return null;
+        WebDriver driver;
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/firefoxDriver/geckodriver.exe");
+        driver = new FirefoxDriver();
+//        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        return driver;
     }
 }
